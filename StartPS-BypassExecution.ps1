c86779c7f7f19fc541547541aee3bdbbf3ccd087
liamwright-HPL <160855944+liamwright-HPL@users.noreply.github.com>
2024-10-21 14:01:23 +0100

    Update StartPS-BypassExecution.ps1

--- StartPS-BypassExecution.ps1
+++ StartPS-BypassExecution.ps1
@@ -1,1 +1,1 @@
-powershell.exe -noprofile -executionpolicy bypass -file C:\temp\Mig_Wiz-main\MigrationWizV3_NoTeams.ps1
+Start-Process "powershell.exe" -ArgumentList "-WindowStyle", "Hidden", "-executionpolicy", "bypass", "-file", "C:\temp\Mig_Wiz-main\MigrationWizV3_NoTeams.ps1", "-NoNewWindow"
